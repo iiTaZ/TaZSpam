3d5953f65dd91b9f9b7d59a57a330e42dce729e0
iiTaZ <45091481+iiTaZ@users.noreply.github.com>
2018-11-18 13:58:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+var prefix = "1";
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -81,6 +82,21 @@ if (message.content === '!spam') {
       }
 });
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 
